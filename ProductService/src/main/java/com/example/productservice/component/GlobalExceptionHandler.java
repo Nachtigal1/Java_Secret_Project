9fe0d3c725bc2e736028e9f4b7686d1ced94a3d3
Nachtigal1 <linder.gleb.cuper@gmail.com>
2026-03-11 13:53:39 +0100

    Add GlobalExceptionHandler for ProductService

---
+++ ProductService/src/main/java/com/example/productservice/component/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.example.productservice.component;
+
+import com.example.productservice.exception.*;
+import com.example.productservice.dto.ExceptionResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({
+            ProductNotFoundException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleNotFoundExceptions(RuntimeException ex) {
+        log.warn("Not found exception: {}", ex.getMessage());
+        return buildExceptionResponse(ex, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({
+            DataIntegrityViolationException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleDatabaseException(RuntimeException ex) {
+        log.warn("Database exception: {}", ex.getMessage());
+        return buildExceptionResponse(ex, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({
+            ProductAlreadyExistsException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleAlreadyExistsExceptions(RuntimeException ex) {
+        log.warn("Already exists exception: {}", ex.getMessage());
+        return buildExceptionResponse(ex, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleGenericException(Exception ex) {
+        log.error("Unhandled exception occurred: {}", ex.getMessage());
+        return buildExceptionResponse(
+                ex,
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    private ResponseEntity<ExceptionResponse> buildExceptionResponse(Exception ex, HttpStatus status) {
+        ExceptionResponse error = new ExceptionResponse(
+                status.value(),
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(error, status);
+    }
+}
---
+++ ProductService/src/main/java/com/example/productservice/dto/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.example.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ExceptionResponse {
+
+    private int status;
+
+    private String message;
+
+    private LocalDateTime timestamp;
+}
